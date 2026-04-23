1db187f42f2194d32fa164558f655c76637027c4
prisharana2024 <prisha.rana.btech2024@sitpune.edu.in>
2026-04-23 18:22:51 +0530

    Delete InvalidBookException.java

--- InvalidBookException.java
+++
@@ -1,6 +0,0 @@
-public class InvalidBookException extends Exception
-{
-    public InvalidBookException(String message) {
-        super(message); // Call the parent constructor with the custom message
-    }
-}
